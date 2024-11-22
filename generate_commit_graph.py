f374a620157f553be82ba97c87c47dcfc580c34c
Pelayo Palacio Suárez <pelayops1041@gmail.com>
2024-11-22 17:59:03 +0100

    update

--- generate_commit_graph.py
+++ generate_commit_graph.py
@@ -14,15 +14,15 @@ url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/commits'
 response = requests.get(url)
 commits = response.json()
 
-# Extrae las fechas de los commits
-dates = [commit['commit']['author']['date'] for commit in commits]
+# Extrae las fechas y los autores de los commits
+data = [(commit['commit']['author']['date'], commit['commit']['author']['name']) for commit in commits]
 
-# Convierte las fechas a un DataFrame de pandas
-df = pd.DataFrame(dates, columns=['date'])
+# Convierte los datos a un DataFrame de pandas
+df = pd.DataFrame(data, columns=['date', 'author'])
 df['date'] = pd.to_datetime(df['date'])
 
-# Cuenta los commits por día
-commits_per_day = df.groupby(df['date'].dt.date).size()
+# Cuenta los commits por día y por autor, y selecciona los últimos 7 días
+commits_per_day_author = df.groupby([df['date'].dt.date, 'author']).size().unstack(fill_value=0).tail(7)
 
 # Elimina el archivo de la gráfica de commits si existe
 if os.path.exists('commit_graph.png'):
@@ -30,11 +30,12 @@ if os.path.exists('commit_graph.png'):
 
 # Grafica los datos
 plt.figure(figsize=(12, 6))
-commits_per_day.plot(kind='bar', color='skyblue', edgecolor='black')
-plt.title('Commits por día', fontsize=16)
+commits_per_day_author.plot(kind='area', stacked=True, linewidth=2)  # Aumenta el grosor de la línea
+plt.title('Commits por día y autor', fontsize=16)
 plt.xlabel('Fecha', fontsize=14)
 plt.ylabel('Número de commits', fontsize=14)
 plt.xticks(rotation=45, ha='right')  # Rotar las etiquetas de fecha 45 grados y alinearlas a la derecha
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.tight_layout()
 plt.savefig('commit_graph.png')
+plt.show()
