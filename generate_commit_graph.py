070918169c2a114bb9302b834c590d7a1326b579
Pelayo Palacio Suárez <pelayops1041@gmail.com>
2024-11-24 12:55:09 +0100

    update test

--- generate_commit_graph.py
+++ generate_commit_graph.py
@@ -19,10 +19,10 @@ data = [(commit['commit']['author']['date'], commit['commit']['author']['name'])
 
 # Convierte los datos a un DataFrame de pandas
 df = pd.DataFrame(data, columns=['date', 'author'])
-df['date'] = pd.to_datetime(df['date'])
+df['date'] = pd.to_datetime(df['date']).dt.date
 
-# Cuenta los commits por día y por autor, y selecciona los últimos 7 días
-commits_per_day_author = df.groupby([df['date'].dt.date, 'author']).size().unstack(fill_value=0).tail(7)
+# Cuenta los commits por día y por autor
+commits_per_day_author = df.groupby(['date', 'author']).size().unstack(fill_value=0)
 
 # Elimina el archivo de la gráfica de commits si existe
 if os.path.exists('commit_graph.png'):
@@ -35,6 +35,7 @@ plt.title('Commits por día y autor', fontsize=16)
 plt.xlabel('Fecha', fontsize=14)
 plt.ylabel('Número de commits', fontsize=14)
 plt.xticks(rotation=45, ha='right')  # Rotar las etiquetas de fecha 45 grados y alinearlas a la derecha
+plt.gca().xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%d/%m/%Y'))
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.tight_layout()
 plt.savefig('commit_graph.png')
